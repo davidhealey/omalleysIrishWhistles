408a690a01e320d488a661984f4563074d14922c
davidhealey <dave41264@hotmail.com>
2019-03-31 16:40:47 +0100

    Added script to filter notes outside playable range

---
+++ Scripts/ScriptProcessors/irishWhistles/rangeFilter.js
@@ -0,0 +1,29 @@
+const knbLow = Content.addKnob("knbLow", 0, 0);
+knbLow.setRange(0, 127, 1);
+knbLow.set("text", "Low Note");
+
+const knbHigh = Content.addKnob("knbHigh", 150, 0);
+knbHigh.setRange(0, 127, 1);
+knbHigh.set("text", "High Note");function onNoteOn()
+{
+    local n = Message.getNoteNumber();
+	
+    if (n < knbLow.getValue() || n > knbHigh.getValue())
+	    Message.ignoreEvent(true);
+}
+function onNoteOff()
+{
+	
+}
+function onController()
+{
+	
+}
+function onTimer()
+{
+	
+}
+function onControl(number, value)
+{
+	
+}
